c915fdc6e9c4799d8e529e006b0cc8c5fd55d73e
Kirill Bezmaternykh <ka.bezmaternykh@gmail.com>
2024-10-05 21:48:34 +0800

    Finish the Get started guide

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Learning.Avalonia.GetStartedApp.Views;
 
@@ -7,5 +8,19 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void ButtonClicked(object source, RoutedEventArgs args)
+    {
+        if (double.TryParse(Celsius.Text, out var celsius))
+        {
+            var fahrenheit = celsius * (9d / 5d) + 32;
+            Fahrenheit.Text = fahrenheit.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
     }
 }
